fe5a4dcd03a0fd812135a7ae02991dd328bc692c
Johannes May <johannes@may-se.de>
2023-08-20 22:29:25 +0200

    Add support for NHWC dimension order in the model runner
    This change should theoretically allow us to run NHWC models as well as
    NCHW models.
    This is largely untested since the model I tried this with so far does
    not seem to work correctly in wonnx and tract.

--- backend/src/model_runner.rs
+++ backend/src/model_runner.rs
@@ -11,11 +11,40 @@ use wonnx::{
 
 use crate::ChunkSize;
 
+pub enum ModelChannelOrder {
+    /// Batch, Channel, Height, Width order, this is the natural order for NeuraTable
+    NCHW,
+    /// Batch, Height, Width, Channel order, this will cause an additional permutation step
+    NHWC,
+}
+
+impl ModelChannelOrder {
+    fn translate_shape_to_chunksize(&self, shape: Shape) -> ChunkSize {
+        match self {
+            ModelChannelOrder::NCHW => ChunkSize {
+                width: shape.dim(3) as usize,
+                height: shape.dim(2) as usize,
+            },
+            ModelChannelOrder::NHWC => ChunkSize {
+                width: shape.dim(2) as usize,
+                height: shape.dim(1) as usize,
+            },
+        }
+    }
+
+    fn scratchpad_buffer_layout(&self, chunksize: ChunkSize) -> (usize, usize, usize) {
+        match self {
+            ModelChannelOrder::NCHW => (3, chunksize.height, chunksize.width),
+            ModelChannelOrder::NHWC => (chunksize.height, chunksize.width, 3),
+        }
+    }
+}
+
 #[derive(Debug, Error)]
 pub enum ModelRunnerError {
     #[error("The model has too many inputs")]
     ModelInputError,
-    #[error("The models input is unsupported (a [1,3,h,w] shaped input is required")]
+    #[error("The models input is unsupported. A [1,3,h,w] or [1,h,w,3] shaped input is required (NCHW or NHWC).")]
     InvalidInputShape(Shape),
     #[error("Could not read model parameters")]
     ModelParameterError(#[from] DataTypeError),
@@ -45,6 +74,7 @@ pub enum ModelRunnerBackend {
 pub struct ModelRunner {
     backend: ModelRunnerBackend,
     chunksize: ChunkSize,
+    model_channel_order: ModelChannelOrder,
 }
 
 impl ModelRunner {
@@ -52,7 +82,9 @@ impl ModelRunner {
         self.chunksize
     }
 
-    fn get_graph_input(graph: &GraphProto) -> Result<(Shape, String), ModelRunnerError> {
+    fn get_graph_input(
+        graph: &GraphProto,
+    ) -> Result<(Shape, String, ModelChannelOrder), ModelRunnerError> {
         let inputs = graph.get_input();
 
         if inputs.len() > 1 {
@@ -61,11 +93,21 @@ impl ModelRunner {
         let input_shape = inputs[0].get_shape()?;
         let input_name = inputs[0].get_name().to_owned();
 
-        if input_shape.dim(0) != 1 || input_shape.dim(1) != 3 {
+        if input_shape.dim(0) != 1 {
             return Err(ModelRunnerError::InvalidInputShape(input_shape));
         }
 
-        Ok((input_shape, input_name))
+        let channel_order = if input_shape.dim(1) == 3 {
+            log::debug!("NCHW model detected!");
+            ModelChannelOrder::NCHW
+        } else if input_shape.dim(3) == 3 {
+            log::debug!("NHWC model detected!");
+            ModelChannelOrder::NHWC
+        } else {
+            return Err(ModelRunnerError::InvalidInputShape(input_shape));
+        };
+
+        Ok((input_shape, input_name, channel_order))
     }
 
     fn get_matching_output(
@@ -88,13 +130,11 @@ impl ModelRunner {
         let wonnx_model = wonnx::onnx::ModelProto::parse_from_reader(input)?;
 
         let graph = wonnx_model.get_graph();
-        let (input_shape, input_name) = Self::get_graph_input(graph)?;
+        let (input_shape, input_name, model_channel_order) = Self::get_graph_input(graph)?;
+        log::info!("Detected model input shape: {:?}", input_shape);
         let output_name = Self::get_matching_output(graph, &input_shape)?;
         log::info!("Using output {}", &output_name);
-        let chunksize = ChunkSize {
-            height: input_shape.dim(2) as usize,
-            width: input_shape.dim(3) as usize,
-        };
+        let chunksize = model_channel_order.translate_shape_to_chunksize(input_shape);
 
         if !force_tract {
             match Session::from_model(wonnx_model).await {
@@ -104,13 +144,12 @@ impl ModelRunner {
                             session,
                             input_name,
                             output_name,
-                            input_scratchpad: ndarray::Array3::<f32>::zeros((
-                                3,
-                                chunksize.height,
-                                chunksize.width,
-                            )),
+                            input_scratchpad: ndarray::Array3::<f32>::zeros(
+                                model_channel_order.scratchpad_buffer_layout(chunksize),
+                            ),
                         }),
                         chunksize,
+                        model_channel_order,
                     })
                 }
                 Err(err) => {
@@ -131,6 +170,7 @@ impl ModelRunner {
 
         let infer = move |input: &ndarray::Array3<f32>| {
             let shape = input.shape().clone();
+            log::debug!("Running tract model with input shape {:?}", shape);
             let mut result = tract_model
                 .run(tvec![Into::<Tensor>::into(
                     input
@@ -152,13 +192,12 @@ impl ModelRunner {
         Ok(Self {
             backend: ModelRunnerBackend::TractRunner(TractRunner {
                 model: Box::new(infer),
-                input_scratchpad: ndarray::Array3::<f32>::zeros((
-                    3,
-                    chunksize.height,
-                    chunksize.width,
-                )),
+                input_scratchpad: ndarray::Array3::<f32>::zeros(
+                    model_channel_order.scratchpad_buffer_layout(chunksize),
+                ),
             }),
             chunksize,
+            model_channel_order,
         })
     }
 
@@ -166,10 +205,29 @@ impl ModelRunner {
         &mut self,
         input: ndarray::ArrayView3<'a, f32>,
     ) -> Result<ndarray::Array3<f32>, ModelRunnerError> {
-        match &mut self.backend {
-            ModelRunnerBackend::WonnxRunner(runner) => runner.process_chunk(input).await,
-            ModelRunnerBackend::TractRunner(runner) => runner.process_chunk(input).await,
-        }
+        log::debug!("Application order data shape: {:?}", input.shape());
+
+        // Input will be an ArrayView to an array of shape (CHW)
+        let model_order_input = match self.model_channel_order {
+            ModelChannelOrder::NCHW => input,
+            ModelChannelOrder::NHWC => input.permuted_axes([1, 2, 0]),
+        };
+
+        log::debug!("Model order data shape: {:?}", model_order_input.shape());
+
+        let model_output = match &mut self.backend {
+            ModelRunnerBackend::WonnxRunner(runner) => {
+                runner.process_chunk(model_order_input).await?
+            }
+            ModelRunnerBackend::TractRunner(runner) => {
+                runner.process_chunk(model_order_input).await?
+            }
+        };
+
+        Ok(match self.model_channel_order {
+            ModelChannelOrder::NCHW => model_output,
+            ModelChannelOrder::NHWC => model_output.permuted_axes([2, 0, 1]),
+        })
     }
 }
 
